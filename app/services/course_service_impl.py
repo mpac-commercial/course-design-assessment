03eef409572a4c9a18acb3f7559d4f364cb585c4
amirmahdi-alavi <sam.alavi@hotmail.com>
2024-02-25 03:00:09 -0500

    implemetation of enroll_student and dropout_student and endpoints

--- app/services/course_service_impl.py
+++ app/services/course_service_impl.py
@@ -2,7 +2,9 @@ from typing import List
 from app.services.course_service import CourseService
 from app.db.connection import get_db, LocalSession
 from app.models.course import Course
+from app.models.student import Student
 from app.models.assignment import Assignment
+from app.models.student_course import StudentCourse
 
 
 class CourseServiceImpl(CourseService):
@@ -47,12 +49,34 @@ class CourseServiceImpl(CourseService):
       return db_assignemnt
 
   
-  def enroll_student(self, course_id, student_id):
-    return super().enroll_student(course_id, student_id)
-  
+  def get_student_by_id(self, student_id) -> Student:
+    with LocalSession() as session:
+      db_student = session.get(Student, ident=student_id)
+    return db_student
+
+
+  def enroll_student(self, course_id, student_id) -> StudentCourse:
+    with LocalSession() as session:
+      db_student_course = StudentCourse(student_id=student_id, course_id=course_id)
+      session.add(db_student_course)
+      session.commit()
+      session.refresh(db_student_course)
+    return db_student_course
+
+
   def dropout_student(self, course_id, student_id):
-    return super().dropout_student(course_id, student_id)
-  
+    with LocalSession() as session:
+      db_student_course = session.query(StudentCourse).where(
+        StudentCourse.course_id==course_id,
+        StudentCourse.student_id==student_id
+      ).order_by(StudentCourse.student_course_id.desc()).first()
+      
+      session.delete(db_student_course)
+      session.commit()
+
+    return db_student_course
+
+
   def submit_assignment(self, course_id, student_id, assignment_id, grade: int):
     return super().submit_assignment(course_id, student_id, assignment_id, grade)
   
--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from app.services.course_service_impl import CourseServiceImpl
 from fastapi import FastAPI
 from app.schemas.course import CourseView, CourseCreate 
 from app.schemas.assignment import AssignmentCreate, AssignmentView
-from fastapi import Depends
-from sqlalchemy.orm import Session
-from app.db.connection import get_db
-from app.models import course as course_model
+from app.schemas.student import StudentView
+from app.schemas.student_course import StudentCourseView, StudentCourseCreate
 import uvicorn
 from typing import List
 
@@ -51,11 +49,63 @@ if __name__ == "__main__":
     )
 
     db_course = course_service.get_course_by_id(db_assignment.course_id)
-
+    
     return AssignmentView(
       assignment_id=db_assignment.assignment_id,
       course_instance={'course_id':db_course.course_id, 'course_name': db_course.course_name},
       assignment_name=db_assignment.assignment_name
+    )
+  
+  
+  @app.post(path='/student/enroll/', response_model=StudentCourseView)
+  def enroll_student(student_course: StudentCourseCreate):
+    db_student_course = course_service.enroll_student(
+      course_id=student_course.course_id,
+      student_id=student_course.student_id
+    )
+
+    db_student = course_service.get_student_by_id(student_course.student_id)
+    student_instance = StudentView(
+      student_name=db_student.student_name,
+      student_id=db_student.student_id
+      )
+
+    db_course = course_service.get_course_by_id(db_student_course.course_id)
+    course_instance = CourseView(
+      course_id=db_course.course_id,
+      course_name=db_course.course_name
+    )
+
+    return StudentCourseView(
+      student_course_id=db_student_course.student_course_id,
+      course_instance=course_instance, 
+      student_instance=student_instance
+      )
+
+  @app.delete(path='/student/dropout/', response_model=StudentCourseView)
+  def dropout_student(student_course: StudentCourseCreate):
+    delted_student_course = course_service.dropout_student(
+      student_id=student_course.student_id,
+      course_id=student_course.course_id
+    )
+
+    db_student = course_service.get_student_by_id(delted_student_course.student_id)
+    student_instance = StudentView(
+      student_name=db_student.student_name,
+      student_id=db_student.student_id
+    )
+
+    db_course = course_service.get_course_by_id(delted_student_course.course_id)
+    course_instance = CourseView(
+
+      course_name=db_course.course_name,
+      course_id=db_course.course_id
+    )
+
+    return StudentCourseView(
+      student_course_id=delted_student_course.student_course_id,
+      student_instance=student_instance,
+      course_instance=course_instance
     )
 
 
