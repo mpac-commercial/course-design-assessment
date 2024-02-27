848613d908170643d5399c7d35b34449b1a6a165
amirmahdi-alavi <sam.alavi@hotmail.com>
2024-02-27 16:30:57 -0500

    added  schema create script

--- db_create.sql
+++ db_create.sql
@@ -1,50 +1,61 @@
-use lms;
+CREATE DATABASE `lms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+
+
 CREATE TABLE `course` (
-  `id` smallint NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `course_name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 
 CREATE TABLE `student` (
-  `id` smallint NOT NULL,
-  `student_name` varchar(100) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `student_name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 
 CREATE TABLE `assignment` (
-  `id` smallint NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course_id` int NOT NULL,
   `assignment_name` varchar(100) NOT NULL,
-  `course_id` smallint NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `assignment_course_id_idx` (`course_id`),
-  CONSTRAINT `assignment_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `course_id` (`course_id`),
+  CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 
 CREATE TABLE `student_course` (
-  `ID` smallint NOT NULL,
-  `student_id` smallint NOT NULL,
-  `course_id` smallint NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `StudentCourse_course_id_idx` (`course_id`),
-  KEY `StudentCourse_student_id_idx` (`student_id`),
-  CONSTRAINT `StudentCourse_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `StudentCourse_student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`) /*!80000 INVISIBLE */,
+  KEY `ix_student_course_course_id` (`course_id`),
+  KEY `ix_student_course_student_id` (`student_id`),
+  CONSTRAINT `course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`),
+  CONSTRAINT `student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 
 CREATE TABLE `submission` (
-  `id` smallint NOT NULL,
-  `course_id` smallint NOT NULL,
-  `student_id` smallint NOT NULL,
-  `assignment_id` smallint NOT NULL,
-  `grade` tinyint DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `assignment_id` int NOT NULL,
+  `grade` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `submission_course_id_idx` (`course_id`),
-  KEY `submission_student_id_idx` (`student_id`),
-  KEY `submission_assignment_id_idx` (`assignment_id`),
-  CONSTRAINT `submission_assignment_id` FOREIGN KEY (`assignment_id`) REFERENCES `assignment` (`id`),
-  CONSTRAINT `submission_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `submission_student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `assignment_id` (`assignment_id`),
+  KEY `course_id` (`course_id`),
+  KEY `student_id` (`student_id`),
+  CONSTRAINT `submission_ibfk_1` FOREIGN KEY (`assignment_id`) REFERENCES `assignment` (`id`),
+  CONSTRAINT `submission_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`),
+  CONSTRAINT `submission_ibfk_3` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
